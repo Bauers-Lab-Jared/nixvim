1aec113bf419f9f38f7dca800e011f3639aecffc
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-05-31 18:45:42 -0400

    built lib for assembling nix configs

---
+++ nix/lib/default.nix
@@ -0,0 +1,64 @@
+{
+  # This is the merged library containing your namespaced library as well as all libraries from
+  # your flake's inputs.
+  lib,
+  # Your flake inputs are also available.
+  inputs,
+  # The namespace used for your flake, defaulting to "internal" if not set.
+  # namespace,
+  # Additionally, Snowfall Lib's own inputs are passed. You probably don't need to use this!
+  # snowfall-inputs,
+}: rec {
+  extendWithList = {
+    thisNixvim,
+    configList,
+  }: let 
+    inherit (builtins) elemAt length;
+  in if (length configList) > 0 then let
+    nextList = if (length configList) > 1 then
+      lib.sublist 1 100 configList
+    else [];
+
+    nextNixvim = thisNixvim.nixvimExtend (elemAt configList 0);
+  in extendWithList { thisNixvim = nextNixvim; configList = nextList; }
+  else thisNixvim;
+
+  extendWithNameList = thisNixvim: nameList: let
+    inherit (lib.snowfall.fs) get-snowfall-file;
+    inherit (builtins) map filter tryEval pathExists toString concatStringsSep;
+
+    getConfig = n: let
+      fixPath = p: concatStringsSep "/" (
+        lib.sublist 4 10 (
+          lib.splitString "/" (
+            toString p)));
+
+      path = (get-snowfall-file "modules/nixos/${n}");
+      try = if pathExists path then tryEval (import path)
+        else (lib.warn "Nixvim config not found: ${fixPath path}" 
+          {success = false; value = "noPath";});
+    in
+      if try.success then try.value
+      else if try.value != "noPath" then
+          (lib.warn "Nixvim config failed to import: ${fixPath path}" null)
+        else null;
+
+    configList = filter (a: a != null) (map getConfig nameList);
+  in
+    extendWithList {inherit thisNixvim configList;};
+
+  mkNixvim = {
+    pkgs,
+    addons ? [],
+  }: let
+    inherit (lib.snowfall.fs) get-snowfall-file;
+    inherit (builtins) getEnv;
+
+    username = getEnv "USER";
+    user-configList = ["user-${username}"] ++ addons;
+    nixvim =
+      import
+      (get-snowfall-file "packages/nixvim") {inherit inputs pkgs;};
+  in
+    extendWithNameList nixvim user-configList;
+}
--- nix/shells/default/default.nix
+++ nix/shells/default/default.nix
@@ -3,7 +3,8 @@
   # as well as the libraries available from your flake's inputs.
   lib,
   # You also have access to your flake's inputs.
-  inputs,
+  # inputs,
+  namespace,
   # All other arguments come from NixPkgs. You can use `pkgs` to pull shells or helpers
   # programmatically or you may add the named attributes as arguments here.
   pkgs,
@@ -11,19 +12,7 @@
   ...
 }:
 mkShell {
-  packages = let
-    inherit (lib.snowfall.fs) get-file;
-
-    username = lib.removeSuffix "\n" (builtins.readFile (get-file ".user"));
-
-    nixvim-base =
-      if username != ""
-      then pkgs.nixvim.nixvimExtend (import (get-file "modules/nixos/user-${username}"))
-      else pkgs.nixvim;
-
-    nixvim-final =
-      nixvim-base.nixvimExtend (import (get-file "modules/nixos/proj-nix"));
-  in [
-    nixvim-final
+  packages = [
+    (lib.${namespace}.mkNixvim {inherit pkgs;})
   ];
 }
