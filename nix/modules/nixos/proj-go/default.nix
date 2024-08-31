418bf089707e0ba5ae51df7aff5293ae8cefdbd0
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-08-31 18:44:36 -0400

    added more go config

--- nix/modules/nixos/proj-go/default.nix
+++ nix/modules/nixos/proj-go/default.nix
@@ -3,5 +3,22 @@
     lsp.servers = {
       gopls.enable = true;
     };
+    conform-nvim.formattersByFt.go = [
+      "goimports"
+      "gofmt"
+      "golines"
+    ];
+    none-ls = {
+      sources = {
+        code_actions.gomodifytags.enable = true;
+        completion.tags.enable = true;
+        diagnostics.golangci_lint.enable = true;
+        formatting = {
+          gofmt.enable = true;
+          goimports.enable = true;
+          golines.enable = true;
+        };
+      };
+    };
   };
 }
