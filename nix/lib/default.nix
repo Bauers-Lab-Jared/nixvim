529698b42b8242ab7ebd2109331a720aab541245
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-08-31 15:12:40 -0400

    added default user config for fallback when user not known

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -12,36 +12,58 @@
   extendWithList = {
     thisNixvim,
     configList,
-  }: let 
+  }: let
     inherit (builtins) elemAt length;
-  in if (length configList) > 0 then let
-    nextList = if (length configList) > 1 then
-      lib.sublist 1 100 configList
-    else [];
+  in
+    if (length configList) > 0
+    then let
+      nextList =
+        if (length configList) > 1
+        then lib.sublist 1 100 configList
+        else [];
 
-    nextNixvim = thisNixvim.nixvimExtend (elemAt configList 0);
-  in extendWithList { thisNixvim = nextNixvim; configList = nextList; }
-  else thisNixvim;
+      nextNixvim = thisNixvim.nixvimExtend (elemAt configList 0);
+    in
+      extendWithList {
+        thisNixvim = nextNixvim;
+        configList = nextList;
+      }
+    else thisNixvim;
 
   extendWithNameList = thisNixvim: nameList: let
     inherit (lib.snowfall.fs) get-snowfall-file;
     inherit (builtins) map filter tryEval pathExists toString concatStringsSep;
 
     getConfig = n: let
-      fixPath = p: concatStringsSep "/" (
-        lib.sublist 4 10 (
-          lib.splitString "/" (
-            toString p)));
+      fixPath = p:
+        concatStringsSep "/" (
+          lib.sublist 4 10 (
+            lib.splitString "/" (
+              toString p
+            )
+          )
+        );
 
-      path = (get-snowfall-file "modules/nixos/${n}");
-      try = if pathExists path then tryEval (import path)
-        else (lib.warn "Nixvim config not found: ${fixPath path}" 
-          {success = false; value = "noPath";});
+      path = get-snowfall-file "modules/nixos/${n}";
+      try =
+        if pathExists path
+        then tryEval (import path)
+        else if lib.hasPrefix "user-" n
+        then
+          (lib.warn "User config not found: ${fixPath path}\nUsing default."
+            (tryEval (import (get-snowfall-file "modules/nixos/user-default"))))
+        else
+          (lib.warn "Nixvim config not found: ${fixPath path}"
+            {
+              success = false;
+              value = "noPath";
+            });
     in
-      if try.success then try.value
-      else if try.value != "noPath" then
-          (lib.warn "Nixvim config failed to import: ${fixPath path}" null)
-        else null;
+      if try.success
+      then try.value
+      else if try.value != "noPath"
+      then (lib.warn "Nixvim config failed to import: ${fixPath path}" null)
+      else null;
 
     configList = filter (a: a != null) (map getConfig nameList);
   in
